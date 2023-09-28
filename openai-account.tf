b7d765dd36125138946ec85706f375fc18222a7d
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-09-28 10:48:34 +0200

    Initial commit

--- openai-account.tf
+++ openai-account.tf
@@ -22,19 +22,3 @@ resource "azurerm_cognitive_account" "this" {
   }
 }
 
-# Create the Azure Cognitive Deployment
-resource "azurerm_cognitive_deployment" "this" {
-  name                 = "kopicloud-acd"
-  cognitive_account_id = azurerm_cognitive_account.this.id
-  
-  model {
-    format  = "OpenAI"
-    name    = var.cognitive_deployment_name
-    version = var.cognitive_deployment_version
-  }
-
-  scale {
-    type = var.cognitive_deployment_scale_type
-  }
-}
-
